dd7156c747dcf8a61d1f8ee3b315012abd8b18f0
Rain120 <chao1202016@outlook.com>
2021-07-17 21:28:16 +0800

    feat: webpack build script

---
+++ packages/webpack/scripts/build.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const configFactory = require('../config/webpack.config');
+
+// Generate configuration
+const config = configFactory('production');
+
+function build() {
+  console.log('Creating an optimized production build...');
+
+  const complier = webpack(config);
+
+  return new Promise((resolve, reject) => {
+    complier.run((err, stats) => {
+      let message;
+
+      if (err) {
+        if (!err.message) {
+          return reject(err);
+        }
+      }
+    });
+  });
+}
+
+build();
